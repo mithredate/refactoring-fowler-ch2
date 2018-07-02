f0bb492b6002db203e23fe261cd7df5489d960ad
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2018-07-02 17:34:30 +0430

    Added unit test for regular type with 5 days

--- tests/CustomerStatementTest.php
+++ tests/CustomerStatementTest.php
@@ -12,14 +12,27 @@ class CustomerStatementTest extends \PHPUnit\Framework\TestCase
     public function testRegularTypeRentalWith2Days()
     {
         $movie = new Movie("Dances with the wolves", Movie::REGULAR);
-
         $rental = new Rental($movie, 2);
-
         $customer = new Customer("John Doe");
+
         $customer->addRental($rental);
 
         $this->assertEquals(
             "Rental Record for John Doe \n\t Dances with the wolves \t 2 \nAmount owed is 0 \nYou earned 1 frequent renter points",
+            $customer->statement()
+        );
+    }
+
+    public function testRegularTypeWith5Days()
+    {
+        $movie = new Movie("Dances with the wolves", Movie::REGULAR);
+        $rental = new Rental($movie, 5);
+        $customer = new Customer("John Doe");
+
+        $customer->addRental($rental);
+
+        $this->assertEquals(
+            "Rental Record for John Doe \n\t Dances with the wolves \t 6.5 \nAmount owed is 0 \nYou earned 1 frequent renter points",
             $customer->statement()
         );
     }
