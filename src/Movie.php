e5f47b1161fba44bcb2958da52ae2286844057ae
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2018-07-03 07:36:18 +0430

    Moved Movie::getFrequentRenterPoints to Price

--- src/Movie.php
+++ src/Movie.php
@@ -75,15 +75,6 @@ class Movie
     }
 
     public function getFrequentRenterPoints($daysRented) {
-        $result = 1;
-        // add bonus for a two doy new release rental
-        if(
-            $this->getPriceCode() == Movie::NEW_RELEASE &&
-            $daysRented > 1
-        ) {
-            $result++;
-        }
-
-        return $result;
+        return $this->_price->getFrequentRenterPoints($daysRented);
     }
 }
